0fc3021f5565bfcdc9316c41a2d04405e2e3ebeb
Moowonn <rlaandnsj11@naver.com>
2024-03-15 10:49:32 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 
 import Main from "./components/main/main";
 import Chat from "./components/another/Chat";
+import Kor from "./components/another/Kor";
+import Usa from "./components/another/Usa";
+import Chinese from "./components/another/Chinese";
+import Japanese from "./components/another/Japanese";
+
 
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -11,6 +16,10 @@ function App() {
       <Routes>
         <Route path="/Main" element={<Main />} />
         <Route path="/Chat" element={<Chat />} />
+        <Route path="/kor" element={<Kor />} />
+        <Route path="/Usa" element={<Usa />} />
+        <Route path="/chinese" element={<Chinese />} />
+        <Route path="/Japanese" element={<Japanese />} />
       </Routes>
     </Router>
   );
